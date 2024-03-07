694db877757894b5d68ea3c8a16bb6476d7cd5d5
Marc-Antoine Robert <47530953+marc-antoinerobert@users.noreply.github.com>
2024-03-07 10:07:53 -0500

    Update JenkinsCreateFeatureBranch.js

--- TamperMonkey/development/JenkinsCreateFeatureBranch.js
+++ TamperMonkey/development/JenkinsCreateFeatureBranch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jenkins create feature branch
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @author       mplante
 // @match        https://jenkins.omnimed.com/job/CreateFeatureBranch/build?delay=0sec
 // @match        https://jenkins.omnimed.com/*/job/CreateFeatureBranch/build?delay=0sec
@@ -15,7 +15,7 @@
     var currentLength = 0
     var branchNameInput = document.getElementsByName('value')[0];
     var div = document.createElement('div');
-    var maxLength = 38;
+    var maxLength = 37;
 
     branchNameInput.addEventListener("input", updateLength);
 
