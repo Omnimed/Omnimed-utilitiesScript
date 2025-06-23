a29ec1331541ec8ee14e054915ce95d32d2d1327
Marc-Antoine Robert <47530953+marc-antoinerobert@users.noreply.github.com>
2025-06-23 16:31:48 -0400

    add qa colors jenkinsIntegrationTestPimper.js

--- TamperMonkey/development/jenkinsIntegrationTestPimper.js
+++ TamperMonkey/development/jenkinsIntegrationTestPimper.js
@@ -1,12 +1,31 @@
 // ==UserScript==
 // @name         Jenkins Integration Test List Pimper
 // @namespace    http://tampermonkey.net/
-// @version      5.0
+// @version      6
 // @description  Jenkins Integration Test List Pimper
-// @author       mcormier
+// @author       mcormier, marobert
 // @match        https://jenkins.omnimed.com/*/job/*/*/
 // @grant        none
 // ==/UserScript==
+
+// color test by team
+const jq = jQuery.noConflict();
+
+jq(document).ready(function() {
+  const styles = `
+    .qaDos { background-color: blue !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaMed { background-color: black !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaReq { background-color: red !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaMad { background-color: #D8780D !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaPor { background-color: green !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaInt { background-color: darkred !important; color: white !important; display: inline-block; padding: 2px 4px; }
+    .qaAll { background-color: grey !important; color: white !important; display: inline-block; padding: 2px 4px; }
+  `;
+
+  jq('<style type="text/css"></style>').text(styles).appendTo('head');
+});
+
+// Remove cukes from failed test
 function openAllFailedTest() {
     if (document.getElementById('showLink') != null){
     document.getElementById('showLink').click();
@@ -70,6 +89,113 @@ function reduceSizeOfElement(element) {
     element.style.fontSize = "normal";
 }
 
+
+//*[@id="failedtestresult"]/tbody/tr[1]/td[1]/a[3]/text()
+
+function colorTestTagForQA(tag, qa) {
+    const links = document.querySelectorAll('#failedtestresult a[href*="' + tag + '"]');
+    if (links.length > 0) {
+        links.forEach(link => {
+            link.parentElement.querySelector('a').classList.remove('qaDos', 'qaReq', 'qaMed', 'qaMad', 'qaPor', 'qaInt');
+            link.parentElement.querySelector('a').classList.add('qa' + qa);
+        });
+    } else {
+        console.warn('Test tag "' + tag + '" does not exist in any link');
+    }
+}
+
+
+function colorTestTags() {
+	var qa = '';
+
+	//Équipe Dossier
+	qa = 'Dos';
+	colorTestTagForQA('antecedent', qa);
+	colorTestTagForQA('clinical.access', qa);
+	colorTestTagForQA('form', qa);
+	colorTestTagForQA('clinicalnote', qa);
+	colorTestTagForQA('diseasefollowup', qa);
+	colorTestTagForQA('immunization', qa);
+	colorTestTagForQA('socialcontext', qa);
+	colorTestTagForQA('allergy', qa);
+	colorTestTagForQA('consultation', qa);
+	colorTestTagForQA('notification', qa);
+	colorTestTagForQA('vitalsign', qa);
+	colorTestTagForQA('vitalsign', qa);
+	colorTestTagForQA('userworkload', qa);
+	colorTestTagForQA('report.clinical', qa);
+	colorTestTagForQA('clinicalnoteactionlog', qa);
+
+	//Équipe Médico admin
+	qa = 'Mad';
+	colorTestTagForQA('calendar', qa);
+	colorTestTagForQA('patient', qa);
+	colorTestTagForQA('patientdocumentgroup', qa);
+	colorTestTagForQA('relationship', qa);
+	colorTestTagForQA('activity', qa);
+	colorTestTagForQA('availability', qa);
+	colorTestTagForQA('event', qa);
+	colorTestTagForQA('institutionpatienttag', qa);
+	colorTestTagForQA('mergepatientrequest', qa);
+	colorTestTagForQA('patientadministrativerecord', qa);
+	colorTestTagForQA('patientcontactinformation', qa);
+	colorTestTagForQA('provisionalpatient', qa);
+	colorTestTagForQA('timeslotwindow', qa);
+	colorTestTagForQA('waitingroom', qa);
+
+	 //Équipe Requetes et resultats
+	qa = 'Req';
+	colorTestTagForQA('safir', qa);
+    colorTestTagForQA('result', qa);
+    colorTestTagForQA('adt', qa);
+    colorTestTagForQA('document', qa);
+    colorTestTagForQA('ramq', qa);
+    colorTestTagForQA('request', qa);
+    colorTestTagForQA('order', qa);
+    colorTestTagForQA('file', qa);
+    colorTestTagForQA('route', qa);
+    colorTestTagForQA('labo', qa);
+    colorTestTagForQA('push', qa);
+
+
+    //Équipe portail patient
+	qa = 'Por';
+	colorTestTagForQA('task', qa);
+	colorTestTagForQA('institutioncontact', qa);
+
+	//Équipe Médication
+	qa = 'Med';
+	colorTestTagForQA('bff.dsq', qa);
+	colorTestTagForQA('communication', qa);
+	colorTestTagForQA('dictionary', qa);
+	colorTestTagForQA('customization', qa);
+	colorTestTagForQA('medication', qa);
+	colorTestTagForQA('prescription', qa);
+
+    //Équipe facturation
+    qa = 'Med';
+	colorTestTagForQA('privatebilling', qa);
+
+
+	//Équipe Intégration
+	qa = 'Int';
+	//----SessionOnimed----
+	colorTestTagForQA('Authorization', qa);
+    colorTestTagForQA('accesslayer', qa);
+    colorTestTagForQA('broker', qa);
+    colorTestTagForQA('healthcareworker', qa);
+    colorTestTagForQA('identity', qa);
+    colorTestTagForQA('accessinstitution', qa);
+    colorTestTagForQA('foreign', qa);
+    colorTestTagForQA('institution', qa);
+    colorTestTagForQA('webapp', qa);
+    colorTestTagForQA('patient.search', qa);
+
+	//All
+	qa = 'All'
+
+}
+
 openAllFailedTest();
-//depending on the page the list can be on various postions
 parseAndCleanList();
+colorTestTags();
