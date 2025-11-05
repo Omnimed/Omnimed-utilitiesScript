1c1a94dbd29ae86e2c5996a1075a7f59d08c9a4e
Jean-Charles Perron <jcperron@omnimed.com>
2025-11-05 11:29:52 -0500

    Update JupyterSetup.sh
    Revert change to install uv

--- JupyterHub/JupyterSetup.sh
+++ JupyterHub/JupyterSetup.sh
@@ -38,17 +38,3 @@ sed -i '$i \  ,\"env\": {\n \    \"LD_LIBRARY_PATH\": \"/home/jovyan/instantclie
 
 sudo apt update
 sudo apt-get install -y libpq-dev
-
-
-# uv Install
-if [ ! -d "/home/jovyan/.local/share/uv" ]; then
-  sudo curl -LsSf https://astral.sh/uv/0.7.21/install.sh | sh
-fi
-
-# Python Kernel Install
-if [ ! -e "$(uv python find 3.12.11)" ]; then
-  uv python install 3.12.11
-  uv venv /home/jovyan/.venv31211 --python 3.12.11
-  uv pip install -p /home/jovyan/.venv31211/bin/python ipykernel pip
-  uv run -p /home/jovyan/.venv31211/bin/python -m ipykernel install --name python3.12.11 --user
-fi
