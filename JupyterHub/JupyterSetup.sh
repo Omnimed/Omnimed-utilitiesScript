bb3cfb48ffc841ba7c17fcb62e6feb473dc21201
Dominic Duchêne <dduchene@omnimed.com>
2026-08-25 16:16:15 -0400

    Comment out default library installations
    Comment out the installation of default libraries in JupyterSetup.sh.

--- JupyterHub/JupyterSetup.sh
+++ JupyterHub/JupyterSetup.sh
@@ -61,17 +61,15 @@ if [ ! -d "/home/jovyan/.local/share/uv/python/cpython-3.12.11-linux-x86_64-gnu"
 fi
 
 # Install all the default libs.
-uv pip install -p $venv_folder/bin/python3 \
-google-genai==1.57.0 \
-minio==7.2.20 \
-mlflow==3.1.1 \
-openai==2.14.0 \
-pandas==2.3.3 \
-plotly==6.5.1 \
-matplotlib==3.10.8 \
-python-dotenv==1.2.1 \
-scikit-learn==1.8.0 \
-vertexai==1.71.1 \
-tqdm==4.67.1
-
-return 0
+# uv pip install -p $venv_folder/bin/python3 \
+# google-genai==1.57.0 \
+# minio==7.2.20 \
+# mlflow==3.1.1 \
+# openai==2.14.0 \
+# pandas==2.3.3 \
+# plotly==6.5.1 \
+# matplotlib==3.10.8 \
+# python-dotenv==1.2.1 \
+# scikit-learn==1.8.0 \
+# vertexai==1.71.1 \
+# tqdm==4.67.1
